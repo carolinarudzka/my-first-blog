ab28a6030839bf89c531a226e059da6c5025441d
carolinarudzka <carolinarudzka@gmail.com>
2016-12-10 14:34:46 +0100

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qst5thacmlow-hx965^^bfwz8=h(2i*5=^qej^o^8=ugusc+g)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carolinarudzka.pythonanywhere.com']
 
 
 # Application definition
